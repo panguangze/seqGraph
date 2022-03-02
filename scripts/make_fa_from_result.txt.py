170d201d12a578bc6488543d3d826275e9b42911
caronkey <guangzep@gmail.com>
2022-03-03 01:04:12 +0800

    fix overlap error

--- scripts/make_fa_from_result.txt.py
+++ scripts/make_fa_from_result.txt.py
@@ -1,9 +1,11 @@
 from Bio import SeqIO
 import sys
+from Bio.Seq import Seq
 fain = sys.argv[1]
 orderin = open(sys.argv[2])
 faout= open(sys.argv[3],"w")
 record_dict = SeqIO.to_dict(SeqIO.parse(fain, "fasta"))
+n_seq = Seq("N"*40)
 for line in orderin.readlines():
     seq = ""
     tmp = line.strip().split("\t")
@@ -11,5 +13,5 @@ for line in orderin.readlines():
         tmp_seq = record_dict[t[0:-1]].seq
         if t[-1] == '-':
             tmp_seq = record_dict[t[0:-1]].seq.reverse_complement()
-        seq = seq + tmp_seq
+        seq = seq +n_seq+ tmp_seq
     faout.write(">"+tmp[0]+"\n"+str(seq)+"\n")
