51615183cecc18d6091e2431f4ae654f0bdf3942
troyeeee <853889815@qq.com>
2022-04-12 14:08:43 +0800

    fix makefa bug

--- scripts/make_fa_from_path.py
+++ scripts/make_fa_from_path.py
@@ -15,41 +15,65 @@ def rev_comp(s):
             sc += 'A'
     return sc
 
-if __name__ =="__main__":
+
+if __name__ == "__main__":
     fa = pysam.FastaFile(sys.argv[1])
     res = open(sys.argv[2], "w")
     paths = open(sys.argv[3], 'r')
     # fa = pysam.FastaFile("/Users/troye/Documents/Recycler/bin/sim800/all.fa")
     # res = open("/Users/troye/Documents/Recycler/bin/sim800/neo800.fa", "w")
     # paths = open("/Users/troye/Documents/tmp/seqGraph/r.debug.c.522446.test.txt", 'rb')
-    i = 0
-    for line in paths.readlines():
+    for idx, line in enumerate(paths.readlines()):
+        if line.startswith("iter") or line.startswith("self"):
+            continue
         fasta = ""
-        if i % 2 != 0:
-            contigs = line.strip().split('\t')
-            for contig in contigs:
-                # print(contig)
-                if contig[-1]=="+":
-                    print(i, contig[:-1], contig[-1])
-                    try:
-                        fasta += fa.fetch(contig[:-1])
-                    except Exception as e:
-                        name = "_".join(contig[:-1].split('_')[:-1])
-                        print(i, name)
-                        fasta += fa.fetch(name)
-                else:
-                    print(i, contig[:-1], contig[-1])
-                    try:
-                        fasta += rev_comp(fa.fetch(contig[:-1]))
-                    except Exception as e:
-                        name = "_".join(contig[:-1].split('_')[:-1])
-                        print(i, name)
-                        fasta += rev_comp(fa.fetch(name))
-            res.write(">res"+str(i+1)+"_"+str(len(fasta))+"\n")
-            res.write(fasta+"\n")
-        i += 1
+        contigs = line.strip().split('\t')
+        for contig in contigs:
+            # print(contig)
+            if contig[-1] == "+":
+                try:
+                    fasta += fa.fetch(contig[:-1])
+                except Exception as e:
+                    name = "_".join(contig[:-1].split('_')[:-1])
+                    print("not found: ", name)
+                    fasta += fa.fetch(name)
+            else:
+                try:
+                    fasta += rev_comp(fa.fetch(contig[:-1]))
+                except Exception as e:
+                    name = "_".join(contig[:-1].split('_')[:-1])
+                    print("not found: ", name)
+                    fasta += rev_comp(fa.fetch(name))
+        res.write(">res" + str(idx + 1) + "_" + str(len(fasta)) + "\n")
+        res.write(fasta + "\n")
 
-
+    #
+    # i = 0
+    # for line in paths.readlines():
+    #     fasta = ""
+    #     if i % 2 != 0:
+    #         contigs = line.strip().split('\t')
+    #         for contig in contigs:
+    #             # print(contig)
+    #             if contig[-1]=="+":
+    #                 print(i, contig[:-1], contig[-1])
+    #                 try:
+    #                     fasta += fa.fetch(contig[:-1])
+    #                 except Exception as e:
+    #                     name = "_".join(contig[:-1].split('_')[:-1])
+    #                     print(i, name)
+    #                     fasta += fa.fetch(name)
+    #             else:
+    #                 print(i, contig[:-1], contig[-1])
+    #                 try:
+    #                     fasta += rev_comp(fa.fetch(contig[:-1]))
+    #                 except Exception as e:
+    #                     name = "_".join(contig[:-1].split('_')[:-1])
+    #                     print(i, name)
+    #                     fasta += rev_comp(fa.fetch(name))
+    #         res.write(">res"+str(i+1)+"_"+str(len(fasta))+"\n")
+    #         res.write(fasta+"\n")
+    #     i += 1
 
     # chx=fa.fetch("EDGE_1298087_length_128_cov_268.000000")
     # print(chx)
