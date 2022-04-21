6970e66dd4b335973f033f5f833506c812a405a1
caronkey <guangzep@gmail.com>
2022-04-21 18:32:49 +0800

    all seg must in blast_segs

--- scripts/parse2.py
+++ scripts/parse2.py
@@ -77,7 +77,7 @@ for line in inp2:
         #write_juncs.append(" ".join(vs)+"\n")
         # outs.write(" ".join(vs)+"\n")
         tmp[vc].remove(kc)
-    if (vs[1] in blast_segs or vs[3] in blast_segs) or ((vs[1] in relevate_blast_segs or vs[3] in relevate_blast_segs) and float(vs[-1]) >= float(depth)/2 and get_len(vs[1]) <= relevate_edge_len and get_len(vs[3]) <= relevate_edge_len):
+    if (vs[1] in blast_segs and vs[3] in blast_segs) or ((vs[1] in relevate_blast_segs or vs[3] in relevate_blast_segs) and float(vs[-1]) >= float(depth)/2 and get_len(vs[1]) <= relevate_edge_len and get_len(vs[3]) <= relevate_edge_len):
         if vs[1] not in blast_segs and (get_len(vs[1]) > relevate_edge_len or float(vs[-1]) <= float(depth)/2):
             continue
         if vs[3] not in blast_segs and (get_len(vs[3]) > relevate_edge_len or float(vs[-1]) <= float(depth)/2):
