40162f26881bee8ab42259f6091858d725942af6
troyeeee <853889815@qq.com>
2022-05-08 12:47:59 +0800

    recon

--- scripts/recon.py
+++ scripts/recon.py
@@ -5,16 +5,17 @@ import re
 fai = open(sys.argv[1])  # fai
 graph = open(sys.argv[2])  # graph
 segf = open(sys.argv[4])  # 需要从新的segs
-outs = sys.argv[3] # 输出prefix
-depth = float(sys.argv[5]) # samtools 计算而来
+outs = sys.argv[3]  # 输出prefix
+depth = float(sys.argv[5])  # samtools 计算而来
 tmp = {}
 segs = []
+seg_g = {}
 out_juncs = []
 for line in fai:
     vs = line.split("\t")
     a = re.split(":|,|;", vs[0])
     tmp[a[0]] = a[1:]
-ref_name_records ={}
+ref_name_records = {}
 for idx, line in enumerate(segf):
     l = line.strip().split('\t')
     t = re.split(r"[\+\-]", l[0])
@@ -23,11 +24,14 @@ for idx, line in enumerate(segf):
     out_juncs.append([])
     for v in t:
         line_segs.append(v)
-    segs.append(v)
+        seg_g[v] = idx
+    segs.append(line_segs)
+
+out_segs = [[] for i in range(0, len(out_juncs))]
 
 
 def in_segs(seg1, seg2=None):
-    if seg2 == None:
+    if seg2 is None:
         for s in segs:
             if seg1 in s:
                 return 1
@@ -43,8 +47,9 @@ for line in graph:
     vs = line.rstrip().split(" ")
     if vs[0] == "SEG":
         if in_segs(vs[1]):
-            outs.write(line)
+            out_segs[seg_g[vs[1]]].append(line)
         continue
+
     k = vs[1]
     kc = vs[1] + "'"
     if vs[2] == '-':
@@ -76,11 +81,18 @@ for item in tmp.keys():
             out_juncs[in_segs(vs[1], vs[3]) - 1].append(vs)
             # out_juncs.append(vs)
             # out_juncs("JUNC {} {} {} {} {}\n".format(first,fdir,second,sdir,int(depth)/2))
-s_juncs = sorted(out_juncs, key=itemgetter(2))
-i = 1
-for js in out_juncs:
-    outs_f = open(outs + str(i)+ "_"+ref_name_records[i]+".txt", "w")
+# s_juncs = sorted(out_juncs, key=itemgetter(2))
+i = 0
+for js in out_segs:
+    outs_f = open(outs + str(i) + "_" + ref_name_records[i] + ".txt", "w")
     i = i + 1
     for j in js:
+        outs_f.write(" ".join(j) + "\n")
+    outs_f.close()
+i = 0
+for js in out_juncs:
+    outs_f = open(outs + str(i) + "_" + ref_name_records[i] + ".txt", "a")
+    i = i + 1
+    for j in sorted(js):
         outs_f.write(" ".join(j) + "\n")
     outs_f.close()
