7fcfb76cf348ffc5d26b1123687f162f944328a6
panguangze <guangzep@gmail.com>
2022-06-05 16:48:30 +0800

    merge xuedong

--- scripts/get_ref_by_index.py
+++ scripts/get_ref_by_index.py
@@ -1,10 +1,10 @@
 import sys
-from pyfaidx import SeqIO
-
-fa = open(sys.argvs[1])
-fai = open(sys.argvs[2])
-ref_indx=open(sys.argvs[3])
-out_ref = open(sys.argvs[4],"w")
+import re
+from Bio import SeqIO
+fa = open(sys.argv[1])
+fai = open(sys.argv[2])
+ref_indx=open(sys.argv[3])
+out_ref = open(sys.argv[4],"w")
 
 fai_array = []
 for line in fai.readlines():
@@ -14,7 +14,8 @@ for line in fai.readlines():
 record_dict = SeqIO.to_dict(SeqIO.parse(fa, "fasta"))
 
 for line in ref_indx.readlines():
-    t = line.split("\t")
-    ref_name = fai_array[t[1]]
+    t = re.split(r"\s+",line)
+    print(t)
+    ref_name = fai_array[int(t[1])-1]
     out_ref.write(">"+ref_name+"\n")
     out_ref.write(str(record_dict[ref_name].seq)+"\n")
