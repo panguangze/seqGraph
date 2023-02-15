36958224dcac75fef29cc6f9f3fbab496f9a8487
caronkey <guangzep@gmail.com>
2023-02-15 18:24:43 +0800

    fix resolve cycle path

--- main.cpp
+++ main.cpp
@@ -308,7 +308,7 @@ int main(int argc, char *argv[]) {
         }
     }
 //    matching for each connected graph
-    int n = 4;
+    int n = 0;
     g->parseConnectedComponents();
     if (SUB_ONLY != "") return 0;
     std::cout<<"total nodes"<<g->getVertices()->size()<<std::endl;
